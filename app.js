9796d7db960ad74458465ce13266fed284e927fa
Harold Fachin <harold.f415@gmail.com>
2020-08-17 14:09:38 -0400

    parsing request bodies

--- app.js
+++ app.js
@@ -31,12 +31,35 @@ const server = http.createServer( (req, res) => {
   }
 
   if( url === '/message' && method === 'POST' ){
-    fs.writeFileSync('message.txt', 'DUMMY');
-    // Status code 302 means REDIRECTION
+    const body = [];
+    
+    // createServer implicitly creates a listener for us but now we do it on our OWN 
+    // The .on() allows us to listen to certain events
+    // The 'data' event will be fire whenever a new chunck of data is ready to be READ
+    // The 'data' event will trigger an annonymous function for every incoming DATA 
+    req.on('data', (chunk) => {
+      // DATA in this part cannot be worked with 
+      console.log(chunk);
+      body.push(chunk);
+    } );
+    
+    // Registering another EVENT LISTENER
+    // Then 'end' LISTENER will fire once its DONE parsing the INCOMING REQUEST DATA
+    req.on('end', () => {
+      // To work with all those chunks of DATA we must BUFFER them
+      // The BUFFER OBJECT is available GLOBALLY thanks to NODE.js
+      // This will create a NEW BUFFER and will add all the CHUNKS of DATA from the BODY
+      // .toString() is a UTILITY METHOD provided by NODE.js
+      const parsedBody = Buffer.concat(body).toString();
+
+      // DATA here is pushed out as key and keyValue. Separated by = 
+      // This allows us to use it an store the DATA
+      console.log(parsedBody);   
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+
     res.writeHead( 302, {Location: '/'} );
-    // res.writeHead() can be written as follows
-    // res.statusCode( 302 );
-    // res.setHeader( 'Location', '/' );
     return res.end();    
   }
 
