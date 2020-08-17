44302234748668efd63788c1714c36a8df94e94b
Harold Fachin <harold.f415@gmail.com>
2020-08-17 13:26:58 -0400

    routing requests

--- app.js
+++ app.js
@@ -13,25 +13,26 @@ const http = require('http');
 // console.log will return data after we use the browser and go to localhost:3000 and it will keep running until you close it ctrl + c
 const server = http.createServer( (req, res) => {
 
-  // req is an OBJECT
-  // It contains headers - META DATA/ INFO
-  console.log(req.url, req.method, req.headers);
-  
-  // This will attach a HEADER to the response where we will pass META INFO that the TYPE of the CONTENT that will ALSO be part of the RESPONSE is HTML
-  res.setHeader('Content-Type', 'text/html');
+  // Load a page where the user can ENTER some DATA which then we STORE in a file on the SERVER once it is SENT
+  // PARSE the URL
+  const url = req.url;
 
-  // Writes DATA to the RESPONSE
+  if( url === '/' ){
+
+   res.write('<html>');
+   res.write('<head> <title> Enter Data </title> </head>');
+   res.write('<body> <form action="/message" method="POST"> <input type="text" name="message"> <button type="submit"> Send </button> </form> </body>');
+   res.write('</html>');
+   return res.end();   
+
+  }
+
+  res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head> <title> Node.JS App v1 </title> </head>');
   res.write('<body> <h1> Hello World! </h1> </body>');
   res.write('</html>');
-  // We MUST tell NODE when we are done with the RESPONSE
-  // We CANNOT keep writing to the RESPONSE after the following line of code.
   res.end();
-
-  // Program will continue to run because of the EVENT LOOP but this next line of code will EXIT the program when it's done.
-  // Typically we don't want to do this because it also EXITS / QUITS your server.
-  // process.exit();
 
 } );
 
